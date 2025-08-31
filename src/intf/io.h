67bfdabf1802a1be826f65d46d3c864057ce5409
Julian Clot <julianclot123@gmail.com>
2025-08-30 23:49:10 -0600

    update io.h

--- src/intf/io.h
+++ src/intf/io.h
@@ -3,14 +3,21 @@
 
 #include <stdint.h>
 
-static inline uint8_t inb(uint16_t port) {
+static inline uint8_t inb(uint16_t port)
+{
     uint8_t ret;
-    asm volatile ("inb %1, %0" : "=a"(ret) : "Nd"(port));
+    asm volatile("inb %1, %0" : "=a"(ret) : "Nd"(port));
     return ret;
 }
 
-static inline void outb(uint16_t port, uint8_t val) {
-    asm volatile ("outb %0, %1" : : "a"(val), "Nd"(port));
+static inline void outb(uint16_t port, uint8_t val)
+{
+    asm volatile("outb %0, %1" : : "a"(val), "Nd"(port));
+}
+
+static inline void qemu_shutdown()
+{
+    asm volatile("out %0, %1" : : "a"(0), "Nd"(0xF4));
 }
 
 #endif
