216f10794f9ca91a4dd7d6d2a911e5332b518e84
Julian Clot <julianclot123@gmail.com>
2025-08-30 23:49:16 -0600

    update string.h

--- src/intf/string.h
+++ src/intf/string.h
@@ -1,7 +1,7 @@
 #pragma once
 
 /**
- * Compara dos strings
- * @return 0 si son iguales, <0 si str1<str2, >0 si str1>str2
+ * Compare two strings.
+ * @return 0 if equal, negative if str1 < str2, positive if str1 > str2
  */
 int str_compare(const char *str1, const char *str2);
